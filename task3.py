6a8875e1374d03e99cf73fae784f2d73c1e0f68b
Tatyana <krupinova@bk.ru>
2022-11-11 18:26:09 +0300

    исправила

--- task3.py
+++ task3.py
@@ -2,14 +2,8 @@
 # которая выведет список неповторяющихся элементов исходной последовательности.
 
 numbers = [1, 10, 1, 5, 9, 5, 10, 7, 1]
-def new_numbers(numbers):
-    new = []
-
-    for number in numbers:
-        if number in new:
-            continue
-        else:
-            new.append(number)
-    return new
-
-print(new_numbers(numbers))
+new = []
+for i in numbers:
+    if numbers.count(i) < 2:
+        new.append(i)
+print(new)
